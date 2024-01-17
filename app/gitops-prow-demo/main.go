927c7e00103b4b3b47cd08f606d8eb8ef2b897a5
TerrificMao <maodanrong_buaa@163.com>
2024-01-18 01:07:08 +0800

    update go code

--- app/gitops-prow-demo/main.go
+++ app/gitops-prow-demo/main.go
@@ -13,7 +13,7 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello!!!!!!! Prow: %d", Random())
+  fmt.Fprintf(w, "Hello!!!!!!!!!! Prow: %d", Random())
 }
 
 func Random() int {
